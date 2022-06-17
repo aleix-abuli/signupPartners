2169c9584f0f8b965ea93b292c3f9de412686329
aleix-abuli <aleixabuli088@gmail.com>
2022-06-17 13:33:10 +0200

    added user page

--- client/src/pages/UserPage/UserPage.jsx
+++ client/src/pages/UserPage/UserPage.jsx
@@ -8,20 +8,36 @@ const server = process.env.REACT_APP_API_URL;
 export default function UserPage() {
     
     const [partner, setPartner] = useState(null);
+    const [locals, setLocals] = useState(false);
 
     const { id } = useParams();
 
     useEffect(() => {
         axios
         .get(`${server}/partners/${id}`)
-        .then(({ data }) => setPartner(data))
+        .then(({ data }) => {
+            setPartner(data);
+            if (data.locals.length > 0) setLocals(true);
+        })
         .catch((err) => console.log(err));
     }, []);
     
     return(
         <>
-            {partner ? 
-            <h1>Hello, {partner.businessName}</h1>
+            { partner ? 
+            <>
+                <h1>Hello, {partner.businessName}</h1>
+                <h3>Welcome to Glovo Partners.</h3>
+                { locals ?
+                <>
+                    <h3>Your current establishments</h3>
+                </>
+                :
+                <>
+                    <p>Seems like you don't have any registered establishments yet.</p>
+                </>
+                }
+            </>
             :
             <>No partner from db</>
             }
